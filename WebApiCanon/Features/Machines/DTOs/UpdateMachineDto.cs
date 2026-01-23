e1d43771bdb13adca05d791010dfc98ac56b290e
Yefry Mendoza <ymendoza@citylendinginc.com>
2026-01-23 17:32:05 -0500

    Adding dockerfile

--- WebApiCanon/Features/Machines/DTOs/UpdateMachineDto.cs
+++ WebApiCanon/Features/Machines/DTOs/UpdateMachineDto.cs
@@ -3,7 +3,7 @@
     public class UpdateMachineDto
     {
         public string Name { get; set; } = string.Empty;
-        public string sCode { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
         public bool IsActive { get; set; } = true;
     }
 }
